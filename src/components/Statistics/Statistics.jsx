010f3924bc016fd3538e8ea5eb029e20ac67d752
czke33 <czke33@gmail.com>
2024-04-02 18:53:14 +0300

    update part 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,30 +1,44 @@
-import style from './statistics.module.css'
+import style from './statistics.module.css';
+import PropTypes from 'prop-types';
+import { getRandomColor } from './getRandomColor';
+
+
+
 
 function Statistics({ title, stats }) {
     return (
+        <>
         <section className={style.statistics}>
     <h2 className={style.title}>{ title}</h2>
-
-  <ul className={style.statList}>
-    <li className={style.item}>
-      <span className="label">.docx</span>
-      <span className="percentage">4%</span>
-    </li>
-    <li className={style.item}>
-      <span className="label">.mp3</span>
-      <span className="percentage">14%</span>
-    </li>
-    <li className={style.item}>
-      <span className="label">.pdf</span>
-      <span className="percentage">41%</span>
-    </li>
-    <li className={style.item}>
-      <span className="label">.mp4</span>
-      <span className="percentage">12%</span>
-    </li>
-  </ul>
+    <ul className={style.statList}>
+                
+                    {stats.map((stat) => 
+                    <>
+                    
+                            <li className={style.item}
+                                key={stat.id}
+                                style={{ backgroundColor: getRandomColor() }} >
+                                                                    
+                <span className={style.label}>{stat.label }</span>
+                        <span className={style.percentage}>{stat.percentage}%</span> 
+                        </li> 
+                        </>)}
+                      </ul>
 </section>
-    )
+    
+    </>)
+};
+
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Statistics
