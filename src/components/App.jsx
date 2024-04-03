6b29119d61b1d79c864d53812721b67efe13f1cc
czke33 <czke33@gmail.com>
2024-04-03 19:37:23 +0300

    add part 4 hw1
    transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import Statistics from './Statistics/Statistics';
 import friends from '../data/friends.json'
 import FriendsList from './FriendsList/FriendsList'
 
+import Transactions from './Transaction/Transaction';
+import transactions from '../data/transactions.json';
 export const App = () => {
   return (<>
     <Profile
@@ -21,6 +23,7 @@ export const App = () => {
 <Statistics stats={data} />
   
       <FriendsList friends={friends} />;
+      <Transactions items={transactions}/>
   </>
   );
 };
